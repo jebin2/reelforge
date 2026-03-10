097d9eebb4a43f389930b8a7514de015c15616f9
jebin2 <jebineinstein@gmail.com>
2026-03-10 11:23:50 +0530

    rm unawante check

--- reelforge/pipeline/face_tagger.py
+++ reelforge/pipeline/face_tagger.py
@@ -3,15 +3,14 @@ from .. import config
 
 def setup():
     cwd = "/tmp/FaceTagger"
+    utils.setup_git_repo_get_install_pip(
+        repo_url="https://github.com/jebin2/FaceTagger.git",
+        target_path=cwd,
+        pip_name="FaceTagger",
+        requirements_file="requirements_auto_crop.txt"
+    )
     if not utils.dir_exists(cwd):
-        utils.setup_git_repo_get_install_pip(
-            repo_url="https://github.com/jebin2/FaceTagger.git",
-            target_path=cwd,
-            pip_name="FaceTagger",
-            requirements_file="requirements_auto_crop.txt"
-        )
-        if not utils.dir_exists(cwd):
-            raise ValueError("FaceTagger not setup correctly.")
+        raise ValueError("FaceTagger not setup correctly.")
 
     python_path = os.path.expanduser("~/.pyenv/versions/FaceTagger_env/bin/python")
     cmd = f"{python_path} insight_face_manager.py '{cache_path}' real"
