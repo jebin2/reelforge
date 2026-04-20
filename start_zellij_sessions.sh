f4930e3b3dfc8d14a79cb76731a2c8c6b02116ce
jebin2 <jebineinstein@gmail.com>
2026-04-20 23:57:45 +0530

    fix: install zellij if not present

--- start_zellij_sessions.sh
+++ start_zellij_sessions.sh
@@ -2,6 +2,15 @@
 
 sleep 20
 
+if ! command -v zellij &> /dev/null; then
+  echo "Installing zellij..."
+  curl -LO https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
+  tar -xvf zellij-x86_64-unknown-linux-musl.tar.gz
+  chmod +x zellij
+  mv zellij "$HOME/.local/bin/"
+  rm -f zellij-x86_64-unknown-linux-musl.tar.gz
+fi
+
 SESSION_NAME="content"
 LAYOUT_PATH="$HOME/git/reelforge/layouts/content.kdl"
 
