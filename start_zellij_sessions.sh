078cf7fad91d80e89b19f97f0a74068efe33f96f
jebin2 <jebineinstein@gmail.com>
2026-04-21 00:49:38 +0530

    Simplify: use attach -c -f to create session if needed

--- start_zellij_sessions.sh
+++ start_zellij_sessions.sh
@@ -7,7 +7,7 @@ LAYOUT="$HOME/git/reelforge/layouts/content.kdl"
 if zellij list-sessions -s | grep -qx "$SESSION"; then
     zellij attach -f "$SESSION"
 else
-    zellij -s "$SESSION" -l "$LAYOUT"
+    zellij attach -c -f "$SESSION"
     sleep 2
 fi
 
